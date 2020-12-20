89f5f9533bf10b705f42e8c23b2941c174d0122b
aatuolk <Aatu.olkkonen@student.lut.fi>
2020-12-20 15:41:39 +0200

    Small fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());  /* For parsing json  */
 // For connecting to MongoDB database 
 // database uri is stored in .env
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
